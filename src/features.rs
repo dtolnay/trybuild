35efc864df73d5e21ef55dcdb6b385561de0f4ba
David Tolnay <dtolnay@gmail.com>
2020-12-13 19:50:05 -0800

    Resolve clippy manual_range_contains lint

--- src/features.rs
+++ src/features.rs
@@ -91,7 +91,7 @@ fn try_find() -> Result<Vec<String>, Ignored> {
 }
 
 fn is_lower_hex_digit(byte: u8) -> bool {
-    byte >= b'0' && byte <= b'9' || byte >= b'a' && byte <= b'f'
+    (b'0'..=b'9').contains(&byte) || (b'a'..=b'f').contains(&byte)
 }
 
 fn from_json<'de, T, D>(deserializer: D) -> Result<T, D::Error>
