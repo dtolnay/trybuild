db6e88014d36e6c5309efae3574639a9f552c049
David Tolnay <dtolnay@gmail.com>
2021-10-25 18:56:50 -0700

    Add test for issue 136

--- src/tests.rs
+++ src/tests.rs
@@ -269,3 +269,33 @@ Additional crates such as `pyo3-asyncio` can be used to integrate async Rust and
 10 | async fn async_function() {}
    | ^^^^^
 "}
+
+// FIXME: should strip the line numbers on src/handler.rs
+test_normalize! {test_dropshot_required_by
+    DIR="/git/dropshot/dropshot"
+    WORKSPACE="/git/dropshot"
+"
+error[E0277]: the trait bound `QueryParams: schemars::JsonSchema` is not satisfied
+   --> /git/dropshot/dropshot/tests/fail/bad_endpoint4.rs:24:14
+    |
+24  |     _params: Query<QueryParams>,
+    |              ^^^^^^^^^^^^^^^^^^ the trait `schemars::JsonSchema` is not implemented for `QueryParams`
+    |
+note: required by a bound in `dropshot::Query`
+   --> /git/dropshot/dropshot/src/handler.rs:547:48
+    |
+547 | pub struct Query<QueryType: DeserializeOwned + JsonSchema + Send + Sync> {
+    |                                                ^^^^^^^^^^ required by this bound in `dropshot::Query`
+" "
+error[E0277]: the trait bound `QueryParams: schemars::JsonSchema` is not satisfied
+   --> tests/fail/bad_endpoint4.rs:24:14
+    |
+24  |     _params: Query<QueryParams>,
+    |              ^^^^^^^^^^^^^^^^^^ the trait `schemars::JsonSchema` is not implemented for `QueryParams`
+    |
+note: required by a bound in `dropshot::Query`
+   --> src/handler.rs:547:48
+    |
+547 | pub struct Query<QueryType: DeserializeOwned + JsonSchema + Send + Sync> {
+    |                                                ^^^^^^^^^^ required by this bound in `dropshot::Query`
+"}
