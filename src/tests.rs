d37d1cfae10051f73840dd5b6baef3361ec79ffe
David Tolnay <dtolnay@gmail.com>
2023-02-27 01:17:46 -0700

    Move the rustlib-with-githash test next to existing rustlib test

--- src/tests.rs
+++ src/tests.rs
@@ -97,6 +97,46 @@ error[E0599]: no method named `quote_into_iter` found for struct `std::net::Ipv4
   | doesn't satisfy `std::net::Ipv4Addr: quote::to_tokens::ToTokens`
 "}
 
+test_normalize! {test_rust_lib_with_githash "
+error[E0599]: the method `to_cxx_exception` exists for reference `&NonError`, but its trait bounds were not satisfied
+ --> tests/ui/result_no_display.rs:4:19
+  |
+4 |         fn f() -> Result<()>;
+  |                   ^^^^^^^^^^ method cannot be called on `&NonError` due to unsatisfied trait bounds
+...
+8 | pub struct NonError;
+  | ------------------- doesn't satisfy `NonError: std::fmt::Display`
+  |
+  = note: the following trait bounds were not satisfied:
+          `NonError: std::fmt::Display`
+          which is required by `&NonError: ToCxxExceptionDefault`
+note: the trait `std::fmt::Display` must be implemented
+ --> /rustc/c5c7d2b37780dac1092e75f12ab97dd56c30861d/library/core/src/fmt/mod.rs:786:1
+  |
+  | pub trait Display {
+  | ^^^^^^^^^^^^^^^^^
+  = note: this error originates in the macro `::cxx::map_rust_error_to_cxx_exception` (in Nightly builds, run with -Z macro-backtrace for more info)
+" "
+error[E0599]: the method `to_cxx_exception` exists for reference `&NonError`, but its trait bounds were not satisfied
+ --> tests/ui/result_no_display.rs:4:19
+  |
+4 |         fn f() -> Result<()>;
+  |                   ^^^^^^^^^^ method cannot be called on `&NonError` due to unsatisfied trait bounds
+...
+8 | pub struct NonError;
+  | ------------------- doesn't satisfy `NonError: std::fmt::Display`
+  |
+  = note: the following trait bounds were not satisfied:
+          `NonError: std::fmt::Display`
+          which is required by `&NonError: ToCxxExceptionDefault`
+note: the trait `std::fmt::Display` must be implemented
+ --> $RUST/core/src/fmt/mod.rs
+  |
+  | pub trait Display {
+  | ^^^^^^^^^^^^^^^^^
+  = note: this error originates in the macro `::cxx::map_rust_error_to_cxx_exception` (in Nightly builds, run with -Z macro-backtrace for more info)
+"}
+
 test_normalize! {test_type_dir_backslash
     INPUT="tests/ui/compile-fail-3.rs"
 "
@@ -262,46 +302,6 @@ note: the following traits must be implemented
   | | }
   | |_^
   = note: this error originates in the macro `anyhow` (in Nightly builds, run with -Z macro-backtrace for more info)
-"}
-
-test_normalize! {test_rust_lib_with_githash "
-error[E0599]: the method `to_cxx_exception` exists for reference `&NonError`, but its trait bounds were not satisfied
- --> tests/ui/result_no_display.rs:4:19
-  |
-4 |         fn f() -> Result<()>;
-  |                   ^^^^^^^^^^ method cannot be called on `&NonError` due to unsatisfied trait bounds
-...
-8 | pub struct NonError;
-  | ------------------- doesn't satisfy `NonError: std::fmt::Display`
-  |
-  = note: the following trait bounds were not satisfied:
-          `NonError: std::fmt::Display`
-          which is required by `&NonError: ToCxxExceptionDefault`
-note: the trait `std::fmt::Display` must be implemented
- --> /rustc/c5c7d2b37780dac1092e75f12ab97dd56c30861d/library/core/src/fmt/mod.rs:786:1
-  |
-  | pub trait Display {
-  | ^^^^^^^^^^^^^^^^^
-  = note: this error originates in the macro `::cxx::map_rust_error_to_cxx_exception` (in Nightly builds, run with -Z macro-backtrace for more info)
-" "
-error[E0599]: the method `to_cxx_exception` exists for reference `&NonError`, but its trait bounds were not satisfied
- --> tests/ui/result_no_display.rs:4:19
-  |
-4 |         fn f() -> Result<()>;
-  |                   ^^^^^^^^^^ method cannot be called on `&NonError` due to unsatisfied trait bounds
-...
-8 | pub struct NonError;
-  | ------------------- doesn't satisfy `NonError: std::fmt::Display`
-  |
-  = note: the following trait bounds were not satisfied:
-          `NonError: std::fmt::Display`
-          which is required by `&NonError: ToCxxExceptionDefault`
-note: the trait `std::fmt::Display` must be implemented
- --> $RUST/core/src/fmt/mod.rs
-  |
-  | pub trait Display {
-  | ^^^^^^^^^^^^^^^^^
-  = note: this error originates in the macro `::cxx::map_rust_error_to_cxx_exception` (in Nightly builds, run with -Z macro-backtrace for more info)
 "}
 
 test_normalize! {test_pyo3_url
