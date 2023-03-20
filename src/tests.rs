742f7914e604fa346d2d61a3972eb26011089196
David Tolnay <dtolnay@gmail.com>
2023-03-20 04:40:07 -0700

    Touch up test from PR 227

--- src/tests.rs
+++ src/tests.rs
@@ -220,7 +220,7 @@ error[E0277]: the trait bound `Thread: serde::de::Deserialize<'_>` is not satisf
 
 test_normalize! {test_cargo_registry_sparse "
 error[E0308]: mismatched types
-Error:   --> tests/compile-fail/surface_source_interval_badarg.rs:7:25
+  --> tests/compile-fail/surface_source_interval_badarg.rs:7:25
    |
 5  |       let mut df = hydroflow_syntax! {
    |  __________________-
@@ -237,21 +237,21 @@ note: function defined here
    |        ^^^^^^^^
 " "
 error[E0308]: mismatched types
-Error:   --> tests/compile-fail/surface_source_interval_badarg.rs:7:25
-   |
-5  |       let mut df = hydroflow_syntax! {
-   |  __________________-
-6  | |         // Should be a `Duration`.
-7  | |         source_interval(5) -> for_each(std::mem::drop);
-   | |                         ^ expected `Duration`, found integer
-8  | |     };
-   | |_____- arguments to this function are incorrect
-   |
+ --> tests/compile-fail/surface_source_interval_badarg.rs:7:25
+  |
+5 |       let mut df = hydroflow_syntax! {
+  |  __________________-
+6 | |         // Should be a `Duration`.
+7 | |         source_interval(5) -> for_each(std::mem::drop);
+  | |                         ^ expected `Duration`, found integer
+8 | |     };
+  | |_____- arguments to this function are incorrect
+  |
 note: function defined here
-  --> $CARGO/tokio-1.26.0/src/time/interval.rs
-   |
-   | pub fn interval(period: Duration) -> Interval {
-   |        ^^^^^^^^
+ --> $CARGO/tokio-1.26.0/src/time/interval.rs
+  |
+  | pub fn interval(period: Duration) -> Interval {
+  |        ^^^^^^^^
 "}
 
 test_normalize! {test_traits_must_be_implemented "
