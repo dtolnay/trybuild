90d04fa815b2bb48dd03fabe6e9a53e7284b235c
Guillaume Gomez <guillaume1.gomez@gmail.com>
2023-07-22 15:56:38 +0200

    Fix html_root_url

--- src/lib.rs
+++ src/lib.rs
@@ -220,7 +220,7 @@
 //! If you need  more control over the stderr check as well, you can take a
 //! look at `compile_fail_check_sub` and `compile_fail_inline_check_sub`.
 
-#![doc(html_root_url = "https://docs.rs/trybuild/1.0.82")]
+#![doc(html_root_url = "https://docs.rs/trybuild2/1.0.2")]
 #![allow(
     clippy::collapsible_if,
     clippy::default_trait_access,
