32408f9c63c571d4395f07d4aecaccb55f8e315f
David Tolnay <dtolnay@gmail.com>
2024-10-18 09:17:54 -0700

    Release 1.0.101

--- src/lib.rs
+++ src/lib.rs
@@ -227,7 +227,7 @@
 //! components = ["rust-src"]
 //! ```
 
-#![doc(html_root_url = "https://docs.rs/trybuild/1.0.100")]
+#![doc(html_root_url = "https://docs.rs/trybuild/1.0.101")]
 #![cfg_attr(not(check_cfg), allow(unexpected_cfgs))]
 #![allow(
     clippy::collapsible_if,
