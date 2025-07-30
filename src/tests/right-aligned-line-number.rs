6ff60c4918be602c8e5f00186c2777610f0da42c
David Tolnay <dtolnay@gmail.com>
2025-07-29 22:05:57 -0700

    Recognize right aligned line numbers

--- src/tests/right-aligned-line-number.rs
+++ src/tests/right-aligned-line-number.rs
@@ -11,8 +11,8 @@ error[E0412]: cannot find type `F` in this scope
    |          ^ help: a type alias with a similar name exists: `A`
 " "
 error[E0412]: cannot find type `F` in this scope
- --> $DIR/ui-testing-optout.rs:92:10
-  |
+  --> $DIR/ui-testing-optout.rs:92:10
+   |
  4 | type A = B;
    | ----------- similarly named type alias `A` defined here
 ...
